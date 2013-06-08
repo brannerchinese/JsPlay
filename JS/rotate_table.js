23cf4d1bec9f344dd83441fdb403535d3e60c228
David Branner <brannerchinese@gmail.com>
2013-06-08 16:13:29 -0400

    added `this.` to all implicitly global variables, `var` to local variables

--- JS/rotate_table.js
+++ JS/rotate_table.js
@@ -1,30 +1,30 @@
 // rotate_table.js
-// 20130606, works
+// 20130608, works
 // David Prager Branner
 // Given an HTML table of arbitrary size but no merged cells, 
 //   flip along the top-left to bottom right diagonal, so that
 //   horizontal Chinese text becomes vertical and vice versa.
 
 function initialize(){
-  directionVertical = false;
+  this.directionVertical = false;
   reportDirection();
 }
 
 function reportDirection(){
-  tableObject = document.getElementById("theTable");
-  buttonText = "Change to columnar text";
+  var tableObject = document.getElementById("theTable");
+  var buttonText = "Change to columnar text";
   tableObject.setAttribute("style", "border:1px;border-spacing:0px 0px;");
-  if (directionVertical) {
+  if (this.directionVertical) {
     buttonText = "Change to horizontal text";
     // If Chinese text, we also want lines separated by space.
     tableObject.setAttribute("style", "border:1px;border-spacing:20px 0px;");
   }
   document.getElementById("turnButton").innerHTML=buttonText;
-  console.log("direction = vertical?", directionVertical);
+  console.log("direction = vertical?", this.directionVertical);
 }
 
 function turn(){
-  directionVertical = !directionVertical;
+  this.directionVertical = !this.directionVertical;
   reportDirection();
   var tableBody = document.getElementsByTagName('tbody')[0];
   var rowArray = tableBody.getElementsByTagName('tr');
@@ -34,12 +34,12 @@ function turn(){
   for (row=0 ; row<rows ; row++){
     tempRow = document.createElement('tr');
     for (column=0 ; column<columns ; column++){
-      if (directionVertical){index = columns-column-1;}
+      if (this.directionVertical){index = columns-column-1;}
       else {index = column;}
       nextCell = rowArray[index].getElementsByTagName('td')[0];
       tempRow.appendChild(nextCell);
     }
-    if (directionVertical) {tempTableBody.appendChild(tempRow);}
+    if (this.directionVertical) {tempTableBody.appendChild(tempRow);}
     else {tempTableBody.insertBefore(tempRow, tempTableBody.firstChild);}
   }
   tableBody.parentNode.replaceChild(tempTableBody, tableBody);
