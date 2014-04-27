231a95f44a72492dc52b14cdc99ea4d2894311c5
dpb15 <brannerchinese@gmail.com>
2014-04-27 18:28:40 -0400

    begin bounce_ball play

---
+++ BOUNCE_BALL/JS/bounce_ball.js
@@ -0,0 +1,33 @@
+// 20140427
+// David Prager Branner
+
+var hidden = false;
+var loops = 14;
+var g = 1
+
+
+var toggle = function () {
+    if (hidden) {
+        $("#ball").show();
+        hidden = false;
+    } else {
+        $("#ball").hide();
+        hidden = true;
+    }
+};
+
+$(document).ready(function () {
+    $("#button").click(function () {
+        toggle();
+    });
+    $("#button2").click(function () {
+        for (var t = 0; t < loops; t++) {
+            $("#ball").animate({
+                'top': '-=' + ((loops / 2) - t) * Math.pow((loops / 2) - t, 2) + 'px',
+                    'left': '+=12px'
+            }, {
+                easing: "linear"
+            });
+        }
+    });
+});
