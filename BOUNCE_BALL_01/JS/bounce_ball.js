317693959ce209be7bb2c1b03089cbd6661635f1
dpb15 <brannerchinese@gmail.com>
2014-04-27 21:40:30 -0400

    add BOUNCE_BALL_01

--- BOUNCE_BALL_01/JS/bounce_ball.js
+++ BOUNCE_BALL_01/JS/bounce_ball.js
@@ -3,37 +3,32 @@
 
 var hidden = false;
 var loops = 14;
-var g = 1;
-var h = 0;
-var v = 10;
 var updown;
 
 var toggle = function () {
     if (hidden) {
-        $("#ball").show();
-        hidden = false;
+      $("#ball").show();
+      hidden = false;
     } else {
-        $("#ball").hide();
-        hidden = true;
+      $("#ball").hide();
+      hidden = true;
     }
 };
 
-$(document).ready(function () {
-    $("#button").click(function () {
-        toggle();
+$(document).ready(function() {
+    $("#button").click(function() {
+      toggle();
     });
-    $("#button2").click(function () {
-        for (var t = 0; t < loops; t++) {
-          v -= g * t;
-          h += v * (t) - (g * Math.pow(t, 2) / 2);
-          updown = ((loops / 2) - t);
-          change = updown* Math.pow((loops/2)-t, 2);
-            $("#ball").animate({
-                'top': '-=' + change + 'px',
-                    'left': '+=12px'
-            }, {
-                easing: "linear"
-            });
-        }
+    $("#button2").click(function() {
+      for (var t = 0; t < loops; t++) {
+//        updown = Math.round((loops-t) / 2) - 1;
+        if (t > loops/2) {updown=-1;}
+        else {updown=1;}
+        change = updown * Math.pow((loops/2)-t, 2);
+        $("#ball").animate({
+            top: '-=' + change + 'px',
+            left: '+=' + t + 'px'
+        }, {easing: "linear",duration:2000});
+      }
     });
 });
---
+++ BOUNCE_BALL_01/README.md
@@ -0,0 +1,7 @@
+## Notes
+
+In Firefox. v.28.0, Chrome v.34.0.1847.131, iCab v.5.1.1, both functions work.
+
+Next, need to control range of movement so that movement is reasonably fast and covers the expected amount of space.
+
+[end]
