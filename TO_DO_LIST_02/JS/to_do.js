e87fab649eaac4a1786e9d101bd37ac28a2a7173
dpb15 <brannerchinese@gmail.com>
2014-05-14 19:28:23 -0400

    begin TO_DO_LIST_02; working

--- TO_DO_LIST_02/JS/to_do.js
+++ TO_DO_LIST_02/JS/to_do.js
@@ -9,9 +9,19 @@ $(document).ready(function () {
   /* Create ordered list. */
   $('#button').click(function() {
     var content = $('#content').val();
+    if (content === '') {return} else {$('#content').val('')}
     $('#list ol').append('<li>' + content + '</li>');
   });
   /* Add checker for past-due. Past-due is flagged. */
   /* Make sortable. */
   /* Add button for "done". */
+
+/*function checkCR(e) {
+  var evnt = e || window.event;
+  if (evnt.keyCode == 13) {
+    var content = $('#content').val();
+    $('#list ol').append('<li>' + content + '</li>');
+  }*/
 });
+
+
