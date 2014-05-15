daed629d852ac59477b9c0273809d5aa1f87918a
dpb15 <brannerchinese@gmail.com>
2014-05-14 20:17:54 -0400

    add working TO_DO_LIST_02

--- TO_DO_LIST_02/JS/to_do.js
+++ TO_DO_LIST_02/JS/to_do.js
@@ -1,27 +1,36 @@
 $(document).ready(function () {
-  /* Add fading effects to menu.*/
-  $('#menu').mouseenter(function() {
-    $(this).fadeTo('swing', 1);
-  });
-  $('#menu').mouseleave(function() {
-    $(this).fadeTo('swing', 0);
-  });
-  /* Create ordered list. */
-  $('#button').click(function() {
+  function addLI() {
     var content = $('#content').val();
     if (content === '') {return} else {$('#content').val('')}
     $('#list ol').append('<li>' + content + '</li>');
+  }
+
+  function checkCR(e) {
+    var evnt = e || window.event;
+    if (evnt.keyCode == 13) {
+        addLI();
+    }
+  }
+
+  /* Add fading effects to menu.*/
+  $('#menu').mouseenter(function() {
+    $(this).fadeTo('fast', 1);
+  });
+  $('#menu').mouseleave(function() {
+    $(this).fadeTo('slow', 0);
+  });
+  /* Create ordered list. */
+/*  $('#button').click(function() {
+    addLI()
+  }); */
+  $('#form').submit(function () {
+    addLI();
+    return false;
   });
   /* Add checker for past-due. Past-due is flagged. */
   /* Make sortable. */
   /* Add button for "done". */
 
-/*function checkCR(e) {
-  var evnt = e || window.event;
-  if (evnt.keyCode == 13) {
-    var content = $('#content').val();
-    $('#list ol').append('<li>' + content + '</li>');
-  }*/
 });
 
 
